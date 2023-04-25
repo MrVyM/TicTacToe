e99955ea7467b2988b36b22cfb541c6fb90e0b39
vianney.marticou <vianney.marticou@epita.fr>
2023-04-25 22:29:01 +0100

    [STYLES] Edit the styles file

---
+++ styles.js
@@ -0,0 +1,25 @@
+export default {
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    backgroundColor: '#FFEEB0',
+    bluePlayerColor: '#45aaf2',
+    redPlayerColor: '#fc5c65',
+    line: {
+        flexDirection: 'row',
+        padding: 2.5,
+        justifyContent: 'space-between'
+    },
+    square: {
+        fontSize:'50px',
+        borderRadius: 10,
+        height: 100,
+        width: 100,
+        margin: 5,
+        backgroundColor: '#d1d8e0',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+}
